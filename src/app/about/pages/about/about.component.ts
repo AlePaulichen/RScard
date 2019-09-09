e456eb05d1553c1b7583104ca6352ecbfee4f719
Alessandro Paulichen <paulichen.ale@gmail.com>
2019-09-09 10:16:12 -0300

    Generic button created

--- src/app/about/pages/about/about.component.ts
+++ src/app/about/pages/about/about.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ButtonColor } from 'src/app/shared/enums/button-color.enum';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
 })
 export class AboutComponent implements OnInit {
+
+  buttonColor = ButtonColor;
 
   constructor() { }
 
