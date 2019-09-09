bc7fc7359c8e32e37f18c23060defe1eecfb8699
Alessandro Paulichen <paulichen.ale@gmail.com>
2019-09-09 13:47:09 -0300

    Skills module and page created

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact.routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ContactComponent } from './pages/contact.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { ContactInfoComponent } from './components/contact-info/contact-info.component';
 import { LocationMapComponent } from './components/location-map/location-map.component';
---
+++ src/app/skills/components/skill-bar/skill-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-skill-bar',
+    templateUrl: './skill-bar.component.html',
+})
+export class SkillBarComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
