6eb08d3ad4199da7ff1e70dd67c190f7ca8074d8
Alessandro Paulichen <paulichen.ale@gmail.com>
2019-09-11 14:54:05 -0300

    Contact module has been refactored using rscss standards

--- src/app/contact/pages/contact.component.ts
+++ src/app/contact/pages/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { MailContact } from 'src/app/shared/model/mail-contact.model';
 import { SocialIconsColor } from 'src/app/shared/enums/social-icons-color.enum';
 
@@ -7,6 +7,8 @@ import { SocialIconsColor } from 'src/app/shared/enums/social-icons-color.enum';
     templateUrl: './contact.component.html',
 })
 export class ContactComponent implements OnInit {
+    @HostBinding('class.rsc-contact') hostClass = true;
+
     socialIconsColor = SocialIconsColor.GRAY;
 
     constructor() { }
